5f960ca24398b1f5eb18450b041c38934fe39c12
Duk <dhugkar@gmail.com>
2021-11-14 23:05:58 +0530

    Max String at 1st

--- src/com/bridgelabz/find_maximum/FindMaximum.java
+++ src/com/bridgelabz/find_maximum/FindMaximum.java
@@ -1,27 +1,27 @@
 package com.bridgelabz.find_maximum;
 
 public class FindMaximum {
-	static Float[] arr = { 1.1f, 1.2f, 1.3f };
+	static String[] arr = { "Peach", "Apple", "Banana" };
 
 	public static void main(String[] args) {
-		Float max;
+		String max;
 
 		max = findMaximum(arr[0], arr[1], arr[2]);
 		display();
 
-		System.out.println("Maximum number : "+max);
+		System.out.println("Maximum : " + max);
 
 	}
 
 	private static void display() {
-		for (Float a : arr) {
+		for (String a : arr) {
 			System.out.print(a + "  ");
 		}
-		System.out.println(); 
+		System.out.println();
 	}
 
-	private static float findMaximum(Float a, Float b, Float c) {
-		Float max;
+	private static String findMaximum(String a, String b, String c) {
+		String max;
 		if (a.compareTo(b) > 0 || a.compareTo(c) > 0) {
 			max = a;
 		} else if (b.compareTo(c) > 0) {
