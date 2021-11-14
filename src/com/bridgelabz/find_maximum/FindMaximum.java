5e784f7736bdb229dc4c43978464efb28181f188
Duk <dhugkar@gmail.com>
2021-11-15 01:36:46 +0530

    Input: Multiple Print: Maximum

--- src/com/bridgelabz/find_maximum/FindMaximum.java
+++ src/com/bridgelabz/find_maximum/FindMaximum.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.find_maximum;
 
+import java.util.Scanner;
+
 public class FindMaximum<T extends Comparable<T>> {
 
 	T arr[];
@@ -10,30 +12,58 @@ public class FindMaximum<T extends Comparable<T>> {
 
 	public <T extends Comparable<T>> T findMaximum(T[] arr) {
 		T max = arr[0];
-		if (arr[1].compareTo(max) > 0) {
-			max = arr[1]; // y is the largest so far
-			if (arr[2].compareTo(max) > 0) {
-				max = arr[2]; // z is the largest now
-				display(arr, max);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].compareTo(max) > 0) {
+				max = arr[i];
 			}
 		}
+		display(arr, max);
 		return max;
 	}
 
-	private <T> void display(T[] a, T max) {
-		System.out.println("Max of " + a[0] + ", " + a[1] + " and " + a[2] + " \n--> " + max);
+	private static <T extends Comparable<T>> void display(T[] arr,T max) {
 
+		for (T a : arr) {
+			System.out.print(a+"  ");
+		}
+		System.out.println("max ->"+max);  
 	}
 
 	public static void main(String[] args) {
 		FindMaximum max = new FindMaximum();
-		Integer[] intArray = { 1, 2, 3 };
-		Float[] floatArray = { 1.1f, 1.2f, 1.3f };
-		String[] strArray = { "Apple", "Banana", "Peach" };
-
-		max.findMaximum(intArray);
-		max.findMaximum(floatArray);
-		max.findMaximum(strArray);
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Type : \n\t1.Integer\n\t2.Float\n\t3.String");
+		int input = sc.nextInt();
+		System.out.print("Enter total number you want :");
+		int n = sc.nextInt();
+		int i = 0;
+		switch (input) {
+		case 1:
+			Integer[] intArray = new Integer[n];
+			while (i < n) {
+				intArray[i] = sc.nextInt();
+				i++;
+			}
+			max.findMaximum(intArray);
+			break;
+		case 2:
+			Float[] floatArray = new Float[n];
+			while (i < n) {
+				floatArray[i] = sc.nextFloat();
+				i++;
+			}
+			max.findMaximum(floatArray);
+			break;
+		case 3:
+			String[] strArray = new String[n];
+			while (i < n) {
+				strArray[i] = sc.next();
+				i++;
+			}
+			max.findMaximum(strArray);
+			break;
+		default:
+			System.out.println("Enter the valid number");
+		}
 	}
 }
