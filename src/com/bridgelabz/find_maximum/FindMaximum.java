59325e70132da5da4eff8bb970b914eecd45b81b
Duk <dhugkar@gmail.com>
2021-11-14 22:19:19 +0530

    Max Number at 1st

--- src/com/bridgelabz/find_maximum/FindMaximum.java
+++ src/com/bridgelabz/find_maximum/FindMaximum.java
@@ -1,11 +1,34 @@
 package com.bridgelabz.find_maximum;
 
 public class FindMaximum {
+	static Integer[] arr = { 3, 1, 2 };
 
 	public static void main(String[] args) {
+		int max;
 
-		System.out.println("Find Maximum Problem using Generics");
+		max = findMaximum(arr[0], arr[1], arr[2]);
+		display();
 
+		System.out.println("Maximum number : "+max);
+
+	}
+
+	private static void display() {
+		for (int a : arr) {
+			System.out.print(a + "  ");
+		}
+		System.out.println(); 
+	}
+
+	private static int findMaximum(Integer a, Integer b, Integer c) {
+		int max;
+		if (a.compareTo(b) > 0 || a.compareTo(c) > 0) {
+			max = a;
+		} else if (b.compareTo(c) > 0) {
+			max = b;
+		} else
+			max = c;
+		return max;
 	}
 
 }
