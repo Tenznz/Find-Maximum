7135a885eae611342faf9300a89eda478fc3ba44
Duk <dhugkar@gmail.com>
2021-11-14 21:43:49 +0530

    Introduction

---
+++ src/com/bridgelabz/find_maximum/FindMaximum.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.find_maximum;
+
+public class FindMaximum {
+
+	public static void main(String[] args) {
+
+		System.out.println("Find Maximum Problem using Generics");
+
+	}
+
+}
