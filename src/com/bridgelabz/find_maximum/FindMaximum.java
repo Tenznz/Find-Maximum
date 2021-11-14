e7b0b79017e46f872473f071e28738b9b834a339
Duk <dhugkar@gmail.com>
2021-11-14 22:23:48 +0530

    Max Number at 2nd

--- src/com/bridgelabz/find_maximum/FindMaximum.java
+++ src/com/bridgelabz/find_maximum/FindMaximum.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.find_maximum;
 
 public class FindMaximum {
-	static Integer[] arr = { 3, 1, 2 };
+	static Integer[] arr = { 1, 3, 2 };
 
 	public static void main(String[] args) {
 		int max;
